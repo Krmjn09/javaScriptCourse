91da185d45f153224680c3cda53fdf86c4233b6a
Krmjn09 <kmahajan1_be22@thapar.edu>
2023-11-01 14:29:52 +0530

    changes committed

---
+++ IntroToJavaScript.js
@@ -0,0 +1,41 @@
+ var age = 32;
+ console.log(age);
+var name = "Kriti Mahajan";
+console.log("my name is: ", name);
+var obj1 = {
+    name: "Kriti",
+    age: 24,
+    height: 5.11,
+    isGud: false,
+   }
+   console.log("obj ",obj1);
+   console.log("keys ",Object.keys(obj1));
+   console.log("values ",Object.values(obj1));
+   // array
+    var arr = [90, 20, 40, 60, 70, 80];
+   // // index no:0, 1, 2 , 3 , 4, 5
+    console.log(arr);
+    console.log(arr[0]);
+   var names = ["rohan", "kiran", "rohit", "anup"];
+   console.log(names[1]);
+   var hetroArr = ["rohan", 123, {isGud: true}, [20,21,22,23]];
+   console.log(hetroArr);
+
+    console.log(hetroArr[0]);
+    console.log(hetroArr[1]);
+   console.log(hetroArr[2].isGud);
+    console.log(hetroArr[3][2]);
+
+    var obj1 ={
+        name: "Kriti",
+        age: 19
+    };
+     
+    console.log(obj1);
+
+    obj1.name ="Mahajan";
+    obj1.age = 20;
+    console.log(obj1);
+
+   
+   
