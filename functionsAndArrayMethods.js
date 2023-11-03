9f8271a17ca9b37b09661f7bddf85e1c7de15023
Krmjn09 <kmahajan1_be22@thapar.edu>
2023-11-03 23:23:34 +0530

    changes commited

--- functionsAndArrayMethods.js
+++ functionsAndArrayMethods.js
@@ -1,3 +1,14 @@
 // arrow functions
 var sum = (a,b) => console.log(a+b);
-sum(4,5);
+sum(4,5); 
+
+// Array Methodology/Methods
+
+var array=[1,2,3,4,5];
+console.log("current array => ", array);
+
+// 1. map (to create a new array)
+var newArray = array.map((data) => data+2);
+console.log("new array => with map=>", newArray);
+
+
